b53ace3e1aedc23e168f9aef4ef66b0d4a4c72c9
sungshin52 <sungshin52@naver.com>
2023-01-09 13:48:26 +0900

    230109

---
+++ 230109_과제/IO.py
@@ -0,0 +1,62 @@
+# Q1
+
+num = int(input())
+print(num)
+
+# Q2
+
+a, b = list(map(int, input().split()))
+print(a, b)
+
+# Q3
+
+a, b, c = list(map(int, input().split()))
+print(a, b, c)
+
+# Q4
+
+str = input().split()
+
+for string in str:
+    print(string, end = ' ')
+
+print('\n')
+
+# Q5
+
+numbers = list(map(int, input().split()))
+
+for num in numbers:
+    print(num, end = ' ')
+
+print('\n')
+
+# Q6
+
+i, j = list(map(int, input().split()))
+print(i, j)
+
+# Q7
+
+alph = input().split()
+
+for alphabet in alph:
+    print(alphabet, end = ' ')
+
+print('\n')
+
+# Q8
+
+nums = list(map(int, input().split()))
+
+for num in nums:
+    print(num, end = ' ')
+
+print('\n')
+
+# Q9
+
+numbers = list(map(int, input().split()))
+
+for num in numbers:
+    print(num, end = ' ')
